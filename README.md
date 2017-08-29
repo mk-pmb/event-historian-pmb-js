08d0b05e806c1297faafbffd82f51bb958e61b44
mk-pmb <mk-pmb@users.noreply.github.com>
2017-08-29 23:54:40 +0200

    v0.1.0

---
+++ README.md
@@ -0,0 +1,113 @@
+﻿
+<!--#echo json="package.json" key="name" underline="=" -->
+event-historian-pmb
+===================
+<!--/#echo -->
+
+<!--#echo json="package.json" key="description" -->
+Watch a standard node.js EventEmitter so you can be notified of events even
+after they already happened.
+<!--/#echo -->
+
+
+Usage
+-----
+
+from [test/usage.js](test/usage.js):
+
+<!--#include file="test/usage.js" start="  //#u" stop="  //#r"
+  outdent="  " code="javascript" -->
+<!--#verbatim lncnt="70" -->
+```javascript
+var eventHistorian = require('event-historian-pmb'), chronicles,
+  clock = require('event-test-clock-pmb').flavors.quarterSeconds();
+
+function logPanicOnce(why) {
+  test.log({ panic: why });
+  this.onPanic = null;
+  return false;   // false = no more reason to panic, at least for now.
+}
+
+chronicles = eventHistorian({
+  subj: clock,    // Subject to study.
+  name: 'usageTestEH',
+  all: 'dblSec',  // Remember all occurrences of these events.
+    // Event name lists can be an array, or a string with names
+    // separated by space.
+  prev: 'tick fullSec',   // Remember just the latest occurrence.
+  first: 'halfSec',       // Remember just the first occurrence.
+  firstPrev: 'qtrSec',    // Remember first and latest occurrence.
+  singular: 'beginTest',  // Like 'first' but panic if occurs again.
+  onPanic: logPanicOnce,
+});
+test.log('Watching.');
+
+equal(chronicles.asa, chronicles.asSoonAs);
+equal(chronicles.paf, chronicles.previousAndFuture);
+chronicles.paf('beginTest', test.log.l8r(['Test begins.']));
+
+clock.emit('beginTest', 'Godspeed!');
+
+function lateSubscribe() {
+  chronicles.asa('fullSec', test.log.l8r(['late asa full']));
+  chronicles.paf('fullSec', test.log.l8r(['late paf full']));
+}
+
+clock.schedule([ 'abs',
+  4, test.log.l8r('One second passed.'),
+  6, lateSubscribe,
+  13, function () { clock.emit('beginTest', 'again!'); },
+  17, function () { test.verify(); },
+  ]);
+
+test.verify = function () {
+  clock.stop();
+
+  equal(chronicles.counts(), { beginTest: 2,
+    qtrSec: 17, halfSec: 8, fullSec: 4, dblSec: 2 });
+
+  equal.lists(test.log(), [
+    'Watching.',
+    [ 'Test begins.', 'Godspeed!' ],
+    'One second passed.',
+    [ 'late asa full', 1 ],
+    [ 'late paf full', 1 ],
+    [ 'late paf full', 2 ],
+    [ 'late paf full', 3 ],
+    { panic: 'Singular event occurred again: beginTest' },
+    [ 'Test begins.', 'again!' ],
+    [ 'late paf full', 4 ],
+  ]);
+
+  test.log.clear();
+  equal.err(function () { clock.emit('beginTest', 'anew!'); },
+    'Error: [EventHistorian usageTestEH]: panic: ' +
+    'Singular event occurred again: beginTest anew!');
+  equal.lists(test.log(), []);
+
+  test.ok();
+};
+```
+<!--/include-->
+
+
+<!--#toc stop="scan" -->
+
+
+
+Known issues
+------------
+
+* needs more/better tests and docs
+
+
+
+
+&nbsp;
+
+
+License
+-------
+<!--#echo json="package.json" key=".license" -->
+ISC
+<!--/#echo -->
---
+++ eh.js
@@ -0,0 +1,159 @@
+﻿/*jslint indent: 2, maxlen: 80, continue: false, unparam: false, node: true */
+/* -*- tab-width: 2 -*- */
+'use strict';
+
+var EX, PT, arSlc = Array.prototype.slice;
+
+function arLast(a, n) { return (a[(+a.length || 0) - (+n || 1)] || false); }
+function sortedKeys(o) { return Object.keys(o).sort(); }
+function oc0() { return Object.create(null); }
+function fail(why) { throw new Error(why); }
+
+
+PT = (function () {
+  function EventHistorian() { return; }
+  return new EventHistorian();
+}());
+
+
+function mapKV(o, f) {
+  return o && sortedKeys(o).map(function (k) { return f(k, o[k]); });
+}
+
+
+EX = function makeEventHistorian(cfg) {
+  var eh = Object.create(PT);
+  eh.subj = cfg.subj;
+  eh.observers = oc0();
+  eh.had = oc0();
+  mapKV(EX.recModes, function (m) { eh.observe(cfg[m], { recMode: m }); });
+  eh.name = (cfg.name || '');
+  if (cfg.onPanic) { eh.onPanic = cfg.onPanic; }
+  return eh;
+};
+
+
+function incEvCnt(eh, ev, add) {
+  var n = ev.name, had = eh.had[n];
+  if (!had) {
+    had = eh.had[n] = [];
+    had.count = 0;
+  }
+  if ((!had.length) && add) { had[0] = ev; }
+  had.count += 1;
+  return had;
+}
+
+
+EX.recModes = {
+  all: function (eh, ev) { incEvCnt(eh, ev, true).push(ev); },
+  prev: function (eh, ev) { incEvCnt(eh, ev)[0] = ev; },
+  first: function (eh, ev) { incEvCnt(eh, ev, true); },
+  singular: function (eh, ev, opt) {
+    if (incEvCnt(eh, ev, true).count === 1) { return; }
+    if (opt.onDupe && (opt.onDupe(ev) === false)) { return; }
+    eh.panic('Singular event occurred again: ' + ev.name, ev);
+  },
+  firstPrev: function (eh, ev) {
+    var had = incEvCnt(eh, ev);
+    had[had.length && 1] = ev;
+  },
+};
+
+
+EX.replayOne = function (pastEv, hnd) {
+  setImmediate(function () { hnd.apply(pastEv.ctx, pastEv); });
+};
+
+
+PT.toString = function () {
+  return '['.concat(this.constructor.name, ' ', (this.name
+    || ('for ' + String(this.subj))), ']');
+};
+
+
+PT.bind = function (mthd) {
+  var f = this[mthd];
+  if (typeof f !== 'function') { fail('No such method: ' + mthd); }
+  return f.bind.apply(f, [this].concat(arSlc.call(arguments, 1)));
+};
+
+
+PT.panic = function (why) {
+  var a = arguments;
+  if (this.onPanic && (this.onPanic.apply(this, a) === false)) { return; }
+  if (a.length > 1) { why = arSlc.call(a).join(' '); }
+  fail(String(this) + ': panic: ' + why);
+};
+
+
+PT.observe = function (evNames, opt) {
+  if (!opt) { opt = false; }
+  var eh = this, rec = EX.recModes[opt.recMode || 'prev'];
+  if (!rec) { fail('Unsupported recording mode: ' + opt.recMode); }
+  if (evNames.match) { evNames = evNames.match(/\S+/g); }
+  evNames.forEach(function (n) {
+    if (eh.observers[n]) { return; }
+    function obs() {
+      var ev = arSlc.call(arguments);
+      ev.name = n;
+      ev.ctx = this;
+      rec(eh, ev, opt);
+    }
+    eh.observers[n] = obs;
+    eh.subj.on(n, obs);
+  });
+  return eh;
+};
+
+
+PT.counts = function (evName) {
+  var cnt;
+  if (evName !== undefined) {
+    cnt = this.had[evName];
+    return ((!!cnt) && cnt.count);
+  }
+  cnt = {};
+  mapKV(this.had, function (k, v) { cnt[k] = v.count; });
+  return cnt;
+};
+
+
+PT.replayPrevByName = function (evName, hnd) {
+  var eh = this, had = (eh.had[evName] || false);
+  if (!eh.observers[evName]) { fail("Event isn't being observed: " + evName); }
+  if (!had.length) { return false; }
+  EX.replayOne(arLast(had), hnd);
+  return true;
+};
+
+
+PT.asa = PT.asSoonAs = function (evName, hnd) {
+  if (this.replayPrevByName(evName, hnd)) { return this; }
+  this.subj.once(evName, hnd);
+  return this;
+};
+
+
+PT.paf = PT.previousAndFuture = function (evName, hnd) {
+  this.replayPrevByName(evName, hnd);
+  this.subj.on(evName, hnd);
+  return this;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = EX;
